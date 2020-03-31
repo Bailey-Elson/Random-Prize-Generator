5ee7c1f22ca12eedd4f704054ce666755ca17046
Bailey-Elson <Baileymitchellelson@gmail.com>
2020-03-31 08:44:08 +0000

    update

--- Service1/application/__init__.py
+++ Service1/application/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask, request
 import requests
+import os
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+# app.config['MYSQL_HOST'] = os.environ.get('MYSQLHOST')
+# app.config['MYSQL_USER'] = os.environ.get('MYSQLUSER')
+# app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQLPASSWORD')
+# app.config['MYSQL_DB'] = os.environ.get('MYSQLDB')
+# mysql = MySQL(app)
 
 from application import routes
--- Service1/application/routes.py
+++ Service1/application/routes.py
@@ -1,10 +1,23 @@
 from application import app
-from flask import render_template, request
+from flask import render_template, request, Flask 
 import requests
 import random
+from flask_mysqldb import MySQL
+import os
+# app = Flask(__name__)
+app.config['MYSQL_HOST'] = os.environ.get('MYSQLHOST')
+app.config['MYSQL_USER'] = os.environ.get('MYSQLUSER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQLPASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQLDB')
+mysql = MySQL(app)
 
 @app.route('/', methods=['GET'])
 def home():
     response = requests.get('http://localhost:5003/generateprize')
     output_sentence = response.text
+    print(output_sentence)
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO prizes(prize)VALUES(%s);",[output_sentence])
+    mysql.connection.commit()
+    cur.close()
     return render_template('index.html', output_sentence = output_sentence, title = 'Home')
