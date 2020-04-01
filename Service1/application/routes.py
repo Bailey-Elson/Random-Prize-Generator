5b04ba73fc4e6701cbd8857c9af5fbf68eaaee84
Bailey-Elson <Baileymitchellelson@gmail.com>
2020-04-01 09:08:38 +0000

    update

--- Service1/application/routes.py
+++ Service1/application/routes.py
@@ -1,23 +1,18 @@
+from flask import Flask, request, render_template, url_for, redirect
 from application import app
-from flask import render_template, request, Flask 
+from flask_mysqldb import MySQL
 import requests
 import random
-from flask_mysqldb import MySQL
-import os
-# app = Flask(__name__)
-app.config['MYSQL_HOST'] = os.environ.get('MYSQLHOST')
-app.config['MYSQL_USER'] = os.environ.get('MYSQLUSER')
-app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQLPASSWORD')
-app.config['MYSQL_DB'] = os.environ.get('MYSQLDB')
+
 mysql = MySQL(app)
 
 @app.route('/', methods=['GET'])
 def home():
-    response = requests.get('http://localhost:5003/generateprize')
+    response = requests.get('http://service4:5003/generateprize')
     output_sentence = response.text
     print(output_sentence)
     cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO prizes(prize)VALUES(%s);",[output_sentence])
+    cur.execute("INSERT INTO prizes(prize)VALUES(%s)",[output_sentence])
     mysql.connection.commit()
     cur.close()
     return render_template('index.html', output_sentence = output_sentence, title = 'Home')
--- Service4/application/routes.py
+++ Service4/application/routes.py
@@ -4,9 +4,9 @@ import requests
 
 @app.route('/generateprize', methods=['GET'])
 def generateprize():
-    ptype = requests.get('http://localhost:5001/prizetype')
+    ptype = requests.get('http://service2:5001/prizetype')
     print(ptype.text)
-    prize = requests.get('http://localhost:5002/randomprize?ptype='+ptype.text)
+    prize = requests.get('http://service3:5002/randomprize?ptype='+ptype.text)
     
     output_sentence = "you won a "+ ptype.text + " prize, you won " + prize.text
     return output_sentence
