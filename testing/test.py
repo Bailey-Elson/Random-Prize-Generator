5c228784ddd100c90620d3d4edea2c8c0455b74f
Bailey-Elson <Baileymitchellelson@gmail.com>
2020-04-08 15:33:57 +0000

    update

--- testing/test.py
+++ testing/test.py
@@ -13,5 +13,9 @@ def test_service1():
     assert 200 == r.status
 def test_service2():
     http = urllib3.PoolManager()
-    r = http.request('GET', 'http://35.230.134.136:5000')
+    r = http.request('GET', 'http://35.230.134.136:5000/')
+    assert 200 == r.status
+def test_service3():
+    http = urllib3.PoolManager()
+    r = http.request('GET', 'http://35.230.134.136/')
     assert 200 == r.status
